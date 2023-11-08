c1e83f6184c32d1cb09a8a62307b66ed3c6560c9
william-martin0204 <william.martin0204@gmail.com>
2023-11-07 23:01:15 -0500

    Add Problem model

---
+++ database/factories/ProblemFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Problem>
+ */
+class ProblemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(5, true),
+            'description' => fake()->paragraph(3, true),
+            'example_input' => fake()->sentence(8, true),
+            'example_output' => fake()->sentence(8, true),
+            'created_by' => fake()->randomElement([1, 2, 3]),
+        ];
+    }
+}
