ba88675f8b69c03fe2fbd751d24dd162144bb2f6
william-martin0204 <william.martin0204@gmail.com>
2023-11-11 13:51:17 -0500

    Add tags.index view

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -1,3 +1,16 @@
 <x-site-layout>
-    {{$tags}}
+    <h1 class="text-4xl font-extrabold text-center my-5">Tags</h1>
+
+    <ul class="grid grid-cols-2 gap-4">
+        @foreach ($tags as $tag)
+
+        <li class="block p-2 shadow-xl bg-white rounded-lg">
+            <p class="text-xl font-bold">{{ $tag->name }}</p>
+            <p class="font-bold">{{ $tag->problems->count() }} problems</p>
+            <p class="text-gray-500">{{ $tag->description }}</p>
+        </li>
+
+        @endforeach
+    </ul>
+
 </x-site-layout>
