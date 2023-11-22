ede82aacfe0d3f7183a16157221d42f12c0342b8
william-martin0204 <william.martin0204@gmail.com>
2023-11-22 16:32:58 -0500

    Add recommended problems to welcome view

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Problem;
+
 class WelcomeController extends Controller
 {
     public function welcome()
     {
-        return view('welcome');
+        // Get the 4 most solved problems
+        $problems = Problem::withCount(['submissions as accepted_submissions_count' => function ($query) {
+            $query->where('status', 'Accepted');
+            if (auth()->check()) $query->whereNot('user_id', auth()->user()->id);
+        }])
+            ->orderBy('accepted_submissions_count', 'desc')
+            ->take(4)
+            ->get();
+
+        return view('welcome', compact('problems'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,21 @@
 <x-site-layout title="Pro-coder Realm Online Judge">
-    
+
+    <h2 class="text-2xl font-extrabold text-center my-5">Try these problems</h2>
+
+    <div class="flex items-center justify-around">
+        <div class="grid xl:grid-cols-4 lg:grid-cols-2">
+            @foreach ($problems as $problem)
+                <a href="{{ route('problems.show', $problem) }}" class="max-w-sm rounded overflow-hidden shadow-lg p-4">
+                    <div class="font-bold text-lg mb-2">
+                        <p >{{ $problem->id }}-{{ Str::limit($problem->title, 30) }}</p>
+                    </div>
+
+                    <div class="flex flex-col space-between">
+                        <p class="text-gray-700 text-base">{{ Str::limit($problem->description, 200) }}</p>
+                    </div>
+                </a>
+            @endforeach
+        </div>
+    </div>
+
 </x-site-layout>
