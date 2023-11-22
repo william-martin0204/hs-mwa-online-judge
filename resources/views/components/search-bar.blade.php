c07e2d20f58257a78f0a71de26d5d172e4534853
william-martin0204 <william.martin0204@gmail.com>
2023-11-22 16:33:17 -0500

    Remove suggestions from search bar

--- resources/views/components/search-bar.blade.php
+++ resources/views/components/search-bar.blade.php
@@ -1,4 +1,4 @@
-<form class="flex items-center">
+<form class="flex items-center" autocomplete="off">
     <label for="simple-search" class="sr-only">Search</label>
     <div class="relative w-1/2 m-2">
         <input type="text" name="search" value="{{request()->input('search')}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="{{$placeholder}}">
