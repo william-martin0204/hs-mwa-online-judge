93505cdc1a58b46a249aa50f9103370fb826468c
william-martin0204 <william.martin0204@gmail.com>
2024-01-12 01:03:58 -0500

    Config to use postmark

--- config/mail.php
+++ config/mail.php
@@ -59,6 +59,7 @@ return [
 
         'postmark' => [
             'transport' => 'postmark',
+            'message_stream_id' => env('POSTMARK_MESSAGE_STREAM_ID'),
             // 'message_stream_id' => null,
             // 'client' => [
             //     'timeout' => 5,
