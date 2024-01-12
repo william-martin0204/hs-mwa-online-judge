bba0c5f95e9aea007359e98532c2766178db0228
william-martin0204 <william.martin0204@gmail.com>
2024-01-12 01:59:54 -0500

    Improve welcome leaderboard cache

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,6 +28,13 @@ class User extends Authenticatable implements HasMedia, MustVerifyEmail
         {
             $user->clearMediaCollection();
         });
+
+        static::creating(function ($user)
+        {
+            if (User::count() < 10) {
+                Cache::delete('welcome.top_ten_users');
+            }
+        });
     }
 
     /**
