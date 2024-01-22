de4751f05d589c58e0ffb7bff51e12e8b08f0662
william-martin0204 <william.martin0204@gmail.com>
2024-01-22 12:04:32 +0100

    Complete Problem api CRUD

--- app/Http/Controllers/Api/ProblemController.php
+++ app/Http/Controllers/Api/ProblemController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ProblemResource;
 use App\Models\Problem;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProblemController extends Controller
 {
@@ -21,5 +22,60 @@ class ProblemController extends Controller
     public function show(Problem $problem)
     {
         return new ProblemResource($problem);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => ['required', 'string', 'unique:problems', 'min:3', 'max:100'],
+            'tags' => ['array'],
+            'description' => ['required', 'string'],
+            'example_input' => ['required', 'string'],
+            'example_output' => ['required', 'string'],
+            'input_testcases' => ['required', 'file', 'mimetypes:text/plain'],
+            'output_testcases' => ['required', 'file', 'mimetypes:text/plain'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $problem = Problem::create($request->all());
+
+        return new ProblemResource($problem);
+    }
+
+    public function update(Request $request, Problem $problem)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => ['required', 'string', 'unique:problems', 'min:3', 'max:100'],
+            'tags' => ['array'],
+            'description' => ['required', 'string'],
+            'example_input' => ['required', 'string'],
+            'example_output' => ['required', 'string'],
+            'input_testcases' => ['required', 'file', 'mimetypes:text/plain'],
+            'output_testcases' => ['required', 'file', 'mimetypes:text/plain'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $problem->update($request->all());
+
+        return new ProblemResource($problem);
+    }
+
+    public function destroy(Problem $problem)
+    {
+        $problem->delete();
+
+        return response()->json([
+            'message' => 'Problem deleted successfully',
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,23 +19,24 @@ Route::get('/tags/{tag}', [TagController::class, 'show']);
 Route::get('/users', [ProfileController::class, 'index']);
 Route::get('users/{user}', [ProfileController::class, 'show']);
 
-Route::middleware(['auth:sanctum'])->group(function () {
 
-    Route::post('/problems', [ProblemController::class, 'store']);
-    Route::put('/problems/{problem}', [ProblemController::class, 'update']);
-    Route::delete('/problems/{problem}', [ProblemController::class, 'destroy']);
+Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::get('/submissions/{submission}', [SubmissionController::class, 'show']);
     Route::post('/submissions', [SubmissionController::class, 'store']);
-
-    Route::post('/tags', [TagController::class, 'store']);
-    Route::put('/tags/{tag}', [TagController::class, 'update']);
-    Route::delete('/tags/{tag}', [TagController::class, 'destroy']);
 
     Route::put('/users', [ProfileController::class, 'update']);
     Route::delete('/users', [ProfileController::class, 'destroy']);
 });
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+
+Route::middleware(['auth:sanctum', 'is.admin'])->prefix('admin')->group(function () {
+
+    Route::post('/problems', [ProblemController::class, 'store']);
+    Route::put('/problems/{problem}', [ProblemController::class, 'update']);
+    Route::delete('/problems/{problem}', [ProblemController::class, 'destroy']);
+
+    Route::post('/tags', [TagController::class, 'store']);
+    Route::put('/tags/{tag}', [TagController::class, 'update']);
+    Route::delete('/tags/{tag}', [TagController::class, 'destroy']);
+});
